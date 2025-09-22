cea1d81aea5501ebd632e51fcfa25fbf4ac26389
Givi Barateli <giobarateli@gmail.com>
2025-09-22 09:55:07 -0500

    Add Category model
    Add Category class with validation attributes
    
    Introduces a new `Category` class in the `BookWebStore.Models` namespace. The class includes properties for `Id`, `Name`, and `DisplayOrder`, with appropriate data annotations for validation and display. The `Id` serves as the primary key, while `Name` is a required string with a maximum length of 50 characters, and `DisplayOrder` is also required and defined as a string.

---
+++ BookWebStore/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWebStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [Display(Name = "Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "Display Order")]
+        public string DisplayOrder { get; set; }
+    }
+}
