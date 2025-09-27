4b31dbf20948bc5c61e5a3cfc847c15e13493a38
Givi Barateli <giobarateli@gmail.com>
2025-09-27 12:57:52 -0500

    Add category management with database seeding
    Updated CategoryController to retrieve categories from the database.
    Added seeding for initial categories in ApplicationDbContext.
    Modified Index view to display categories in a table format.
    Created migration for seeding category data and updated model snapshot.

--- BookWebStore/Controllers/CategoryController.cs
+++ BookWebStore/Controllers/CategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookWebStore.Data;
+using BookWebStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookWebStore.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        //private static List<Category> _categories = new List<Category>();
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.ToList();
+            
+
+            return View(categories);
         }
     }
 }
--- BookWebStore/Migrations/20250927165922_SeedCategoryTableWithSomeData.Designer.cs
+++ BookWebStore/Migrations/20250927165922_SeedCategoryTableWithSomeData.Designer.cs
@@ -3,6 +3,7 @@ using BookWebStore.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookWebStore.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250927165922_SeedCategoryTableWithSomeData")]
+    partial class SeedCategoryTableWithSomeData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
